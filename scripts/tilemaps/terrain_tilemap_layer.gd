9f7f6bb9c998b112177b06f892241c31a317cdf7
ThreeCoffes <leonard.madzielewski@gmail.com>
2025-11-08 04:32:36 +0100

    refactor

--- scripts/tilemaps/terrain_tilemap_layer.gd
+++ scripts/tilemaps/terrain_tilemap_layer.gd
@@ -1,6 +1,18 @@
 class_name TerrainTilemapLayer extends TileMapLayer
 
-@export var tile_picker: TilePicker
+@export var debug: bool = false
+
+signal faction_picked(faction_id: int)
+
+var selected_city: int = -1
+
+enum TileTypes{
+	NONE,
+	WATER,
+	WOODS,
+	AGRI,
+	CITY,
+}
 
 func global_to_tilemap_coordinates(global_pos):
 	var local_pos = to_local(global_pos)
@@ -10,5 +22,24 @@ func global_to_tilemap_coordinates(global_pos):
 func _unhandled_input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
 		var selected_cell = global_to_tilemap_coordinates(get_global_mouse_position())
-		tile_picker.handle_select_cell(selected_cell)
+		if debug: 
+			print("clicked tilemap layer (%d, %d)" % [selected_cell.x, selected_cell.y])
+
+		handle_select_cell(selected_cell)
+
+func get_cell_type(coords: Vector2i) -> TileTypes:
+	if !get_used_rect().has_point(coords):
+		return TileTypes.NONE
+	return get_cell_atlas_coords(coords).x as TileTypes
 		
+func handle_select_cell(cell_coords: Vector2i):
+	match get_cell_type(cell_coords):
+		TileTypes.CITY:
+			selected_city = CityManager.get_cell_faction_id(cell_coords)
+			faction_picked.emit(selected_city)
+			if debug:
+				print("CITY STANDS AT YOUR COMMAND: ", selected_city)
+		TileTypes.AGRI, TileTypes.WOODS:
+			if selected_city != -1:
+				if debug:
+					print("GET BACK TO WORK")
