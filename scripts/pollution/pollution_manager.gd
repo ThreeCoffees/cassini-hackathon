38a19d483efe2745c3c029b702790d33b1f07e68
domiwskakuj <dominikaszymczak2004@gmail.com>
2025-11-09 02:52:54 +0100

    propaguja sie, ale jeszcze nie idealnie

--- scripts/pollution/pollution_manager.gd
+++ scripts/pollution/pollution_manager.gd
@@ -1,21 +1,25 @@
 class_name PollutionManager 
 extends Node
 
+var pollutions_items : Array[Pollution] = []
 var pollutions : Array[Array] = []
 var width: int = 128
 var height: int = 128
 
 @export var plant_placer : PowerPlantPlacer
-var tilemap_layer : TerrainTilemapLayer
+@export var tilemap_layer : TerrainTilemapLayer
 
 func _ready() -> void:
 	plant_placer.plant_placed.connect(Callable(self, "_on_plant_placed"))
 	prepare_dummy_array()
 
 func add_polution(position : Vector2):
-	var pollution = Pollution.new(0.2, position, 4)
-	pollutions[position.x][position.y] = pollution
+	var arr_position = tilemap_layer.local_to_map(position)
+	var pollution = Pollution.new(0.2, position, 4, self)
+	pollutions[arr_position.x][arr_position.y] = 1
+	pollutions_items.append(pollution)
 	tilemap_layer.add_child(pollution)
+	
 
 func prepare_dummy_array():
 	pollutions.resize(width)
@@ -23,28 +27,35 @@ func prepare_dummy_array():
 		pollutions[i] = []
 		pollutions[i].resize(height)
 		for j in height:
-			pollutions[i][j] = null
+			pollutions[i][j] = 0
 
 func _on_plant_placed(type :PowerPlant.PlantTypes, position : Vector2, tilemap: TerrainTilemapLayer):
-	tilemap_layer = tilemap
+	var arr_position = tilemap_layer.local_to_map(position)
+	# tilemap_layer = tilemap
 	match type:
 		PowerPlant.PlantTypes.WOOD:
-			var pollution = Pollution.new(0.8, position, 2)
-			pollutions[position.x/24][position.y/24] = pollution
+			var pollution = Pollution.new(0.8, position, 2, self)
+			pollutions[arr_position.x][arr_position.y] = 1
+			pollutions_items.append(pollution)
 			tilemap_layer.add_child(pollution)
 		PowerPlant.PlantTypes.SUN:
-			var pollution = Pollution.new(0.5, position, 3)
-			pollutions[position.x/24][position.y/24] = pollution
+			var pollution = Pollution.new(0.5, position, 3, self)
+			pollutions[arr_position.x][arr_position.y] = 1
+			pollutions_items.append(pollution)
 			tilemap_layer.add_child(pollution)
 		PowerPlant.PlantTypes.WIND:
-			var pollution = Pollution.new(0.2, position, 4)
-			pollutions[position.x/24][position.y/24] = pollution
+			var pollution = Pollution.new(0.2, position, 4, self)
+			pollutions[arr_position.x][arr_position.y] = 1
+			pollutions_items.append(pollution)
 			tilemap_layer.add_child(pollution)
 
 func is_polluted(position : Vector2):
-	if (pollutions[position.x][position.y] != null):
+	var arr_position = tilemap_layer.local_to_map(position)
+	if (pollutions[arr_position.x][arr_position.y] == 1):
 		return true
 	return false
 	
-		
+func propagate_all():
+	for pollution in pollutions_items:
+		pollution.try_propagation()
 	
--- scripts/timers/resource_timer.gd
+++ scripts/timers/resource_timer.gd
@@ -2,7 +2,9 @@ class_name ResourceTimer extends Timer
 
 @export var debug: bool = false
 @export var happiness_bar : ProgressBar
+@export var pollution_manager : PollutionManager
 
 func _on_timeout():
 	ResourceManager.calculate_stores()
 	happiness_bar.set_value_no_signal(ResourceManager.get_happiness())
+	pollution_manager.propagate_all()
