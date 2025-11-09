71913834aface2eb441b3ec790271bd17975f998
domiwskakuj <dominikaszymczak2004@gmail.com>
2025-11-09 09:01:02 +0100

    pollution to tilemap and balanced

--- scripts/pollution/pollution_manager.gd
+++ scripts/pollution/pollution_manager.gd
@@ -8,17 +8,19 @@ var height: int = 128
 
 @export var plant_placer : PowerPlantPlacer
 @export var tilemap_layer : TerrainTilemapLayer
+@export var pollution_layer : TileMapLayer
 
 func _ready() -> void:
 	plant_placer.plant_placed.connect(Callable(self, "_on_plant_placed"))
 	prepare_dummy_array()
 
-func add_polution(position : Vector2):
+func add_polution(position : Vector2, value : float):
 	var arr_position = tilemap_layer.map_to_local(position)
-	var pollution = Pollution.new(0.2, arr_position, 4.5, self)
+	pollution_layer.set_cell(position,2, Vector2(0,0))
+	var pollution = Pollution.new(0.2, arr_position, value, self)
 	pollutions[position.x][position.y] = 1
 	pollutions_items.append(pollution)
-	tilemap_layer.add_child(pollution)
+	#tilemap_layer.add_child(pollution)
 
 func generate_pollution_from_array(array : Array[Array]):
 	for i in 128:
@@ -29,7 +31,8 @@ func generate_pollution_from_array(array : Array[Array]):
 				var position = tilemap_layer.map_to_local(arr_position)
 				var pollution = Pollution.new(0.2, position, 4, self)
 				pollutions_items.append(pollution)
-				tilemap_layer.add_child(pollution)
+				pollution_layer.set_cell(arr_position,2, Vector2(0,0))
+				#tilemap_layer.add_child(pollution)
 		
 
 func prepare_dummy_array():
@@ -48,17 +51,20 @@ func _on_plant_placed(type :PowerPlant.PlantTypes, position : Vector2, tilemap: 
 			var pollution = Pollution.new(0.8, position, 3, self)
 			pollutions[arr_position.x][arr_position.y] = 1
 			pollutions_items.append(pollution)
-			tilemap_layer.add_child(pollution)
+			pollution_layer.set_cell(arr_position,2, Vector2(0,0))
+			#tilemap_layer.add_child(pollution)
 		PowerPlant.PlantTypes.SUN:
 			var pollution = Pollution.new(0.5, position, 4, self)
 			pollutions[arr_position.x][arr_position.y] = 1
 			pollutions_items.append(pollution)
-			tilemap_layer.add_child(pollution)
+			pollution_layer.set_cell(arr_position,2, Vector2(0,0))
+			#tilemap_layer.add_child(pollution)
 		PowerPlant.PlantTypes.WIND:
 			var pollution = Pollution.new(0.2, position, 4.5, self)
 			pollutions[arr_position.x][arr_position.y] = 1
 			pollutions_items.append(pollution)
-			tilemap_layer.add_child(pollution)
+			pollution_layer.set_cell(arr_position,2, Vector2(0,0))
+			#tilemap_layer.add_child(pollution)
 
 func is_polluted(position : Vector2):
 	if (pollutions[position.x][position.y] == 1):
